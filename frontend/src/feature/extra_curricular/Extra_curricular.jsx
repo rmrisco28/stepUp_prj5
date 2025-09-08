bce7db7557ee4e87eb8a274dcbb9daf7d5b4cfef
jihoon0213 <jihun8138@gmail.com>
2025-09-08 17:53:18 +0900

    비교과프로그램 임시 검색폼

--- frontend/src/feature/extra_curricular/Extra_curricular.jsx
+++ frontend/src/feature/extra_curricular/Extra_curricular.jsx
@@ -1,10 +1,26 @@
-import { Col, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  Row,
+} from "react-bootstrap";
 
 export function Extra_curricular() {
   return (
-    <Row className="justify-content-center">
+    <Row className="justify-content-center w-100">
       <Col xs={12} md={8} lg={6}>
-        <>비교과</>
+        <h3 className="mb-5">비교과 프로그램</h3>
+        <section className="bg-primary px-3 px-sm-5 py-4 rounded-4">
+          <div>
+            <FormGroup>
+              <FormLabel>검색</FormLabel>
+              <FormControl className="w-100 mb-3" />
+            </FormGroup>
+            <Button variant="outline-light">검색</Button>
+          </div>
+        </section>
       </Col>
     </Row>
   );
