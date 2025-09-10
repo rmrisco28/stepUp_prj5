41cc6e9e779ca3d440769144e1363bcbf4452983
jihoon0213 <jihun8138@gmail.com>
2025-09-10 17:40:48 +0900

    비교과프로그램

--- backend/src/main/java/com/example/backend/extracurricular/controller/ExtraCurricularController.java
+++ backend/src/main/java/com/example/backend/extracurricular/controller/ExtraCurricularController.java
@@ -1,5 +1,6 @@
 package com.example.backend.extracurricular.controller;
 
+import com.example.backend.extracurricular.service.ExtraCurricularService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/extracurricular")
 @RequiredArgsConstructor
 public class ExtraCurricularController {
+
+    private final ExtraCurricularService extraCurricularService;
 }
