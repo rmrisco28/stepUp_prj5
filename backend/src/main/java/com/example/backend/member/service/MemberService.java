7422e06c517e828cd209647cf8bd396245b1428a
yumin359 <yumin359@gmail.com>
2025-09-11 16:14:08 +0900

    dev 넣을거

---
+++ backend/src/main/java/com/example/backend/member/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.example.backend.member.service;
+
+import com.example.backend.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+}
