31675c17a60a73379a96eb45a34c03a924dc1f55
jihoon0213 <jihun8138@gmail.com>
2025-09-18 13:04:57 +0900

    이미지파일 dto생성

---
+++ backend/src/main/java/com/example/backend/extracurricular/dto/ETCImageThumbDto.java
@@ -0,0 +1,13 @@
+package com.example.backend.extracurricular.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ETCImageThumbDto {
+    private String name;
+    private String path;
+}
