8f118858c51be27d3bb6e42e6b8e5b91e1e79ca2
yumin359 <yumin359@gmail.com>
2025-09-18 12:17:39 +0900

    비교과 카드 목록 컴포넌트를 만들어 다른 페이지에서도 가져올 수 있도록 함.

--- frontend/src/common/MainPage.jsx
+++ frontend/src/common/MainPage.jsx
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Spinner } from "react-bootstrap";
 import axios from "axios";
 import { ExtraCurricular } from "../feature/extracurricular/ExtraCurricular.jsx";
 import { AppFooter } from "./AppFooter.jsx";
+import { ExtraCurricularCardList } from "../feature/extracurricular/ExtraCurricularCardList.jsx";
 
 export function MainPage() {
   const [loginStatus, setLoginStatus] =
     useState("로그인 상태를 확인해 주세요.");
+  const [programs, setPrograms] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // programs 데이터를 불러오는 useEffect
+  useEffect(() => {
+    axios
+      .get("/api/extracurricular/list")
+      .then((res) => {
+        setPrograms(res.data.programList);
+      })
+      .catch((err) => console.error("목록 불러오기 실패", err))
+      .finally(() => setLoading(false));
+  }, []); // 빈 배열을 넣어 컴포넌트가 마운트될 때 한 번만 실행되도록 합니다.
+
+  if (loading) {
+    return (
+      <div className="text-center my-5">
+        <Spinner animation="border" />
+      </div>
+    );
+  }
 
   async function handleStatusButton() {
     try {
@@ -38,7 +60,7 @@ export function MainPage() {
       <Button onClick={handleStatusButton}>로그인 상태 확인</Button>
       <p>{loginStatus}</p>
       {/* 캐루셀로 뭐 보이게? 근데 이러면 세션 그거 해야함 !! 흠*/}
-      {/*<ExtraCurricular />*/}
+      <ExtraCurricularCardList programs={programs} />
       <AppFooter />
     </>
   );
