d397f1bc040bf4ffbf9370bd2ce17eeea4db63b2
yumin359 <yumin359@gmail.com>
2025-09-19 15:33:17 +0900

    주석

--- frontend/src/feature/activity/MyETC.jsx
+++ frontend/src/feature/activity/MyETC.jsx
@@ -4,7 +4,8 @@ export function MyETC() {
   return (
     <Row className="justify-content-center">
       <Col xs={12} md={8} lg={6}>
-        <>나의 활동</>
+        <div>나의 활동 : 학생 기본 정보랑 같이 가져올까?</div>
+        <div>로그인 안 된 사람은 로그인 화면으로</div>
       </Col>
     </Row>
   );
