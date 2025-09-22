6665a24ae2f24441324f515a25bf1ad32521ff15
yumin359 <yumin359@gmail.com>
2025-09-22 14:32:13 +0900

    비교과 신청 테이블 컬럼 수정

--- jh_sql/ymsql.sql
+++ jh_sql/ymsql.sql
@@ -1,6 +1,7 @@
 use prj5;
 
 SHOW CREATE TABLE extra_curricular_program;
+SHOW CREATE TABLE extra_curricular_application;
 # 잘못된 참조 관계 삭제
 ALTER TABLE extra_curricular_program
     DROP FOREIGN KEY fk_program_competency;
@@ -15,3 +16,7 @@ ALTER TABLE extra_curricular_program
             REFERENCES sub_competency (sub_competency_seq);
 SET FOREIGN_KEY_CHECKS = 0;
 SET FOREIGN_KEY_CHECKS = 1;
+
+# 신청 상태 변경
+ALTER TABLE extra_curricular_application
+    MODIFY COLUMN status INT(1) NOT NULL DEFAULT 1 COMMENT '0=미신청, 1=신청';
